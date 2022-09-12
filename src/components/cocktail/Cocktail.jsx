fce71e1d058759b56bd27a7647704310f399b1a8
Sivagajan <sivagajan@gmail.com>
2022-09-12 17:16:35 +0200

    Page 2 created

---
+++ src/components/cocktail/Cocktail.jsx
@@ -0,0 +1,14 @@
+import style from './Cocktail.module.css'
+
+const Cocktail = (props) => {
+return(
+    <div className={style.cocktailContainer}>
+        <img src={props.cocktail_Img} className={style.image}></img>
+        <p className={style.cocktailName}>{props.cocktail_Name}</p>
+    </div>
+    )
+    
+
+}
+
+export default Cocktail
