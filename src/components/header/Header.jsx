01ea8dbe14abf0d6724d7aebc076769c369843de
Stanni11 <supercoder@SuperCoders-Air.fritz.box>
2022-09-12 22:50:22 +0200

    header style 3

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,6 @@
 import style from './Header.module.css'
 import { Link } from "react-router-dom"
+import vector from '../../img/Vector.png'
 const Header = () => {
     return (
         <>
@@ -19,7 +20,10 @@ const Header = () => {
                 </div>
 
                 <section className={style.arrows}>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#fff" viewBox="0 0 256 256" class="caret_double_icon"><rect width="256" height="256" fill="none"></rect><polyline points="208 128 128 208 48 128" fill="none" stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></polyline><polyline points="208 48 128 128 48 48" fill="none" stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></polyline></svg>
+                    <img src={vector} alt="" />
+                    <img src={vector} alt="" />
+                    <img src={vector} alt="" />
+
                 </section>
             </header>
         </>
