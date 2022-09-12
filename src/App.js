548b777dcd0bbb52fdfd0df0ea8d6a4538210d8f
Stanni11 <supercoder@SuperCoders-Air.fritz.box>
2022-09-12 14:29:32 +0200

    arrow added

--- src/App.js
+++ src/App.js
@@ -5,11 +5,7 @@ import Header from './components/header/Header.jsx'
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       <img src="" alt="" />
-=======
-      <Header/>
->>>>>>> 3da38974d1f48ced2c10501e31b371725c709e7a
     </div>
   );
 }
