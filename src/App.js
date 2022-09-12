b6d83ddb9e8a41791cda3bfb5ee975dce72fc891
Sivagajan <sivagajan@gmail.com>
2022-09-12 15:40:39 +0200

    Everything cleaned

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-<<<<<<< HEAD
-
-import './App.css';
-import Header from './components/header/Header.jsx'
-=======
 import './App.css';
 import Header from './components/header/Header.jsx'
 import Footer from './components/footer/Footer.jsx'
->>>>>>> cdd1e9681d5d8899618db97dbad000758d922d05
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <Header/>
-=======
       < Header />
       <Footer />
->>>>>>> cdd1e9681d5d8899618db97dbad000758d922d05
     </div>
   );
 }
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,49 +1,27 @@
-<<<<<<< HEAD
-
-=======
->>>>>>> cdd1e9681d5d8899618db97dbad000758d922d05
 import style from './Header.module.css'
 
 const Header = () => {
     return (
         <>
-<<<<<<< HEAD
             <header className   ={style.hero}>
                 <nav className='logo'>
                     <h2>DRINKS & CHILL</h2>
                     <button>MENU</button>
                 </nav>
-                <div className='heading'>
+                <div className={style.heading}>
                     <h1>Cocktails & Getränke! </h1>
                     <p>HERZLICH WILKOMMEN IN DER WELT DER COCKTAILS UND DER GETRÄNKE</p>
                 </div>
 
-                <div className='btn'>
-=======
-            <header>
-                <nav className='heading'>
-                    <h2>DRINKS & CHILL</h2>
-                    <button>MENU</button>
-                </nav>
-                <div>
-                    <h1>Cocktails & Getränke </h1>
-                    <p>HERZLICH WILKOMMEN IN DER WELT DER COCKTAILS UND DER GETRÄNKE</p>
-                </div>
+                <div className={style.btn}>
 
-                <div>
->>>>>>> cdd1e9681d5d8899618db97dbad000758d922d05
-                    <input type="text" />
+                    <input type={style.text} />
                     <button>Search</button>
                 </div>
 
-                <section className='arrows'>
+                <section className={style.arrows}>
                     <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#fff" viewBox="0 0 256 256" class="caret_double_icon"><rect width="256" height="256" fill="none"></rect><polyline points="208 128 128 208 48 128" fill="none" stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></polyline><polyline points="208 48 128 128 48 48" fill="none" stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></polyline></svg>
                 </section>
-<<<<<<< HEAD
-                
-=======
-
->>>>>>> cdd1e9681d5d8899618db97dbad000758d922d05
             </header>
         </>
     )
