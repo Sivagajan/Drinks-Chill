171956271c2fe9fecf82bdf22b8a802b50522b08
HusseinKanaan <107847813+HusseinKanaan@users.noreply.github.com>
2022-09-13 13:33:40 +0200

    Link to GinPage

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,39 +1,50 @@
 import { Link } from "react-router-dom"
+import style from './Home.module.css'
 
 const Home = () => {
 
-    return(
+    return (
         <>
-        <main>
-            <section>
-                <article className="style1 left">
-                    <a>
-                        <h2>Gin</h2>
-                        <p>Let the evening BeGin</p>
-                    </a>
-                </article>
-                <article className="style2 right">
-                    <h2>Vodka</h2>
-                    <p>if life gives you Lemons, add Vodka</p>
-                </article>
-                <article className="style3 left">
-                    <h2>Rum</h2>
-                    <p>Rum Forrest Rum</p>
-                </article>
-                <article className="style4 right">
-                    <h2>Scotch</h2>
-                    <p>Scotch is like liquid sunshine</p>
-                </article>
-                <article className="style5 left">
-                    <h2>Non-Alocohlic</h2>
-                    <p>Time for a break</p>
-                </article>
-                <article className="style6 right">
-                    <h2>Random</h2>
-                    <p>the strugle is Real</p>
-                </article>
-            </section>
-        </main>
+            <main>
+                <section>
+                    <article className="style1 left">
+                        <Link to='/cocktailList'>
+                            <h2>Gin</h2>
+                            <p>Let the evening BeGin</p>
+                        </Link>
+                    </article>
+                    <article className="style2 right">
+                        <Link to='/cocktailList'>
+                            <h2>Vodka</h2>
+                            <p>if life gives you Lemons, add Vodka</p>
+                        </Link>
+                    </article>
+                    <article className="style3 left">
+                        <Link to='/cocktailList'>
+                            <h2>Rum</h2>
+                            <p>Rum Forrest Rum</p>
+                        </Link>
+                    </article>
+                    <article className="style4 right">
+                        <Link to='/cocktailList'>
+                            <h2>Scotch</h2>
+                            <p>Scotch is like liquid sunshine</p>
+                        </Link>
+                    </article>
+                    <article className="style5 left">
+                        <Link to='/cocktailList'>
+                            <h2>Non-Alocohlic</h2>
+                            <p>Time for a break</p>
+                        </Link>
+                    </article>
+                    <article className="style6 right">
+                        <Link to='/cocktailList'>
+                            <h2>Random</h2>
+                            <p>the strugle is Real</p>
+                        </Link>
+                    </article>
+                </section>
+            </main>
         </>
 
     )
