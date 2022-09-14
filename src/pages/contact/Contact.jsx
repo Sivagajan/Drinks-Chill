560f90a7fa4a78530b1d8f1086283dae34feff42
Sivagajan <100611372+Sivagajan@users.noreply.github.com>
2022-09-14 19:06:33 +0200

    Stanislav1 (#15)
    * asdasdf
    
    * asfasfa
    
    * animation button
    
    * commit 44
    
    * 23 commits
    
    * chris weint popup erstellt, noch nicht fertig
    
    Co-authored-by: Stanni11 <supercoder@SuperCoders-Air.fritz.box>

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,9 +1,12 @@
 import Header from '../../components/header/Header'
 import Footer from '../../components/footer/Footer'
 import './Contact.css'
-import chris from '../../img/chris.png'
+import { useState } from 'react';
+import ChrisWeint from '../../components/chrisWeint/ChrisWeint';
 
 const Contact = () => {
+
+    const [popupBtn1, setpopupBtn1] = useState(false)
 
     const place = 'noSearch'
     return (
@@ -11,8 +14,8 @@ const Contact = () => {
         < Header place={place} />
             <div className="contactBackground">
                 <h3 className="h3Contact">Contact</h3>
-                <p className="pContact">Du hast Bugs auf unserer schönen Webiste entdeckt? Dann maile doch entweder an christian.peters@supercode.de oder benutze das Kontaktformular um uns wissen zu lassen wie wir unseren Coding Guru Christian noch mehr Verzweifelung fühlen lassen können.</p>
-                <img src={chris} alt="" />
+                <p className="pContact">Du hast Bugs auf unserer schönen Webiste entdeckt? Dann maile doch entweder an christian.peters@supercode.de oder benutze das Kontaktformular um uns wissen zu lassen wie wir unseren    Coding Guru Christian noch mehr Verzweifelung fühlen lassen können.</p>
+
 
                 <form className='formContact' action="">
                     <label className='ContactLabel' htmlFor="">Ihr Name</label>
@@ -21,7 +24,8 @@ const Contact = () => {
                     <input className='contactInput2' type="email" name="" id="" />
                     <label className='ContactLabel' htmlFor="">Ihre Nachricht</label>
                     <input className='contactInput3' type="text" />
-                    <button className='btnContact'>Submit</button>
+                    <ChrisWeint trigger={popupBtn1} setTrigger={setpopupBtn1}></ChrisWeint>
+                    <button onClick={() => setpopupBtn1(true)} className='btnContact'>Submit</button>
                 </form>
             </div>
             <Footer />
